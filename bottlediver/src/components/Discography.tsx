131af6cd5fee259d057825999b15bfbbd6e1025e
Kento Yamamoto <key7135g@gmail.com>
2024-12-02 00:59:24 +0900

    レスポンシブ設計

--- bottlediver/src/components/Discography.tsx
+++ bottlediver/src/components/Discography.tsx
@@ -4,20 +4,48 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid'
+import Grid from '@mui/material/Grid2'
 
 
 const Discography = () => {
     return (
-        <Box sx={{width: '80%', marginInline: 'auto'}}>
+        <Box sx={{ width: '80%', marginInline: 'auto' }}>
             <Box fontWeight="fontWeightLight" fontSize="32px" sx={{ padding: '30px 0 30px 0' }}>
                 Discography
             </Box>
             <Divider variant="middle" />
-            <Box sx={{width: '70%', paddingTop: '10px', marginInline: 'auto', textAlign: 'left'}}>
-                <Grid xs={12}>
-                    未明
-                </Grid>
+            <Box sx={{ maxWidth: '600px', margin: '10% auto 10% auto', justifyContent: 'center', display: 'flex', alignItems: 'center' }}>
+                <Box sx={{ width: '20%', textAlign: 'left', display: 'flex', alignItems: 'center' }}>
+                    <img src="2nd.jpg" style={{ width: '100%' }} />
+                </Box>
+                <Box sx={{ marginLeft: '12%', textAlign: 'left', Width: '80%' }}>
+                    <Box fontWeight="fontWeightLight" sx={{ fontSize: { xs: '15px', sm: '20px' }, padding: '5px 0 5px 0' }}>
+                        2nd Single 『未明』
+                    </Box>
+                    <Box fontWeight="fontWeightLight" sx={{ fontSize: { xs: '12px', sm: '15px' } }}>
+                        - M1. 未明
+                    </Box>
+                    <Box fontWeight="fontWeightLight" sx={{ fontSize: { xs: '12px', sm: '15px' } }}>
+                        - M2. STROBE
+                    </Box>
+                </Box>
+            </Box>
+            <Divider variant="middle" />
+            <Box sx={{ maxWidth: '600px', margin: '10% auto 10% auto', justifyContent: 'center', display: 'flex', alignItems: 'center' }}>
+                <Box sx={{ width: '20%', textAlign: 'center', display: 'flex', alignItems: 'center' }}>
+                    <img src="1st.jpg" style={{ width: '100%' }} />
+                </Box>
+                <Box sx={{ marginLeft: '12%', textAlign: 'left', Width: '80%' }}>
+                    <Box fontWeight="fontWeightLight" sx={{ fontSize: { xs: '15px', sm: '20px' }, padding: '5px 0 5px 0' }}>
+                        1st Single 『ordinary』
+                    </Box>
+                    <Box fontWeight="fontWeightLight" sx={{ fontSize: { xs: '12px', sm: '15px' } }}>
+                        - M1. 一閃
+                    </Box>
+                    <Box fontWeight="fontWeightLight" sx={{ fontSize: { xs: '12px', sm: '15px' } }}>
+                        - M2. 日々
+                    </Box>
+                </Box>
             </Box>
         </Box>
     )
