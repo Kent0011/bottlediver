a3c9da81f6a4ed9a1ba0af183b9c88ca9fa30a6a
Kento Yamamoto <key7135g@gmail.com>
2024-12-07 00:43:06 +0900

    詳細の改善

--- bottlediver/src/components/Footer.tsx
+++ bottlediver/src/components/Footer.tsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box'
 import Divider from "@mui/material/Divider"
 import { FaReact } from "react-icons/fa";
 import { SiMui } from "react-icons/si";
+import { IoLogoVercel } from "react-icons/io5";
 
 const Footer = () => {
     return (
@@ -11,7 +12,7 @@ const Footer = () => {
                 © 2024 bottle diver
             </Box>
             <Box fontSize="12px" sx={{ paddingBottom: '3px' }}>
-                Powered by  <FaReact style={{ margin: '0 -1px -1px 3px' }} /> React  <SiMui style={{ margin: '0 0 -1px 3px' }} /> MUI
+                Powered by  <FaReact style={{ margin: '0 -1px -1px 3px' }} /> React  <SiMui style={{ margin: '0 0 -1px 3px' }} /> MUI and  <IoLogoVercel style={{ margin: '0 -2px -1px 3px' }} /> Vercel
             </Box>
         </Box>
     )
