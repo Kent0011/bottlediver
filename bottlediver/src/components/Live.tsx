eeb272b763a36d1373d8aad24e45830f2800ac4b
Kento Yamamoto <key7135g@gmail.com>
2024-12-12 17:15:53 +0900

    live欄を更新

--- bottlediver/src/components/Live.tsx
+++ bottlediver/src/components/Live.tsx
@@ -12,15 +12,40 @@ import Typography from '@mui/material/Typography';
 import Backdrop from '@mui/material/Backdrop';
 import LiveContent from './LiveContent';
 
+const modalstyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: {xs: '90%', sm: '70%'},
+    height: 'fit-content',
+    bgcolor: 'black',
+    color: '#14202c',
+    boxShadow: 24,
+    p: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+};
+
 
 const Live = () => {
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
 
     return (
         <Box sx={{ width: { xs: '90%', md: '80%' }, marginInline: 'auto' }}>
             <FadeAnimation>
-                <Box fontWeight="fontWeightLight" fontSize="32px" sx={{ padding: '30px 0 30px 0' }}>
+                <Box fontWeight="fontWeightLight" fontSize="32px" sx={{ padding: '30px 0 20px 0' }}>
                     Live
+                </Box>
+            </FadeAnimation>
+            <FadeAnimation>
+                <Box sx={{ display: 'flex', width: {xs: '90%', sm:'70%'}, justifyContent: 'space-around', margin: '0 auto' }}>
+                    <Button href="https://docs.google.com/forms/d/e/1FAIpQLSfeHoMQok0EVnG7Q-TaPyHf6Q_UIypQ4EIS538oW-J7fCPUdw/viewform?usp=sf_link" sx={{ margin: '0 0 15px 0', fontSize: '18px' }}>TICKET</Button>
+                    <Button onClick={handleOpen} sx={{ margin: '0 0 15px 0', fontSize: '18px' }}>GOODS</Button>
                 </Box>
             </FadeAnimation>
             <FadeAnimation>
@@ -64,6 +89,26 @@ const Live = () => {
                     <Divider variant="middle" />
                 </FadeAnimation>
             </Box>
+
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                slots={{ backdrop: Backdrop }}
+                slotProps={{
+                    backdrop: {
+                        timeout: 500,
+                    },
+                }}
+            >
+                <Fade in={open}>
+                    <Box fontWeight="fontWeightLight" sx={modalstyle}>
+                        <img src='goods.png' style={{width: '100%'}}/>
+                    </Box>
+                </Fade>
+            </Modal>
         </Box>
     )
 }
